665a06725113429b65bb6f4db233b906c088c2e6
Tim King <tim.kinger@gmail.com>
2022-06-27 11:16:58 -0500

    delete support files

--- includes/templates/home-column1-ad.php
+++
@@ -1,6 +0,0 @@
-<?php /* Column #1 Ad */
-if( get_option('newspaper_column1_ad_enable') == 'on'){
-	echo "<div class='home-column1-ad'>";
-	echo get_option('newspaper_column1_ad_code');
-	echo "</div>";
-} ?>
